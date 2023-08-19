13850ec79e1977cd670a60b7e2c944e39c3130a7
karinabluu <141613782+karinabluu@users.noreply.github.com>
2023-08-19 15:33:45 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,6 +1,6 @@
 function solution(n) {
     // 숫자를 문자열로 변환하여 각 자릿수를 배열에 저장
-    var digits = n.toString().split('').map(Number);
+    var digits = n.toString().split('');
 
     // 내림차순으로 정렬
     digits.sort(function(a, b) {
