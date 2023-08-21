0befce05c362597945b826b989bf5f441108fca5
karinabluu <141613782+karinabluu@users.noreply.github.com>
2023-08-21 16:39:14 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,13 +1,10 @@
 function solution(phone_number) {
     var answer = '';
-    var pnum = phone_number.slice(-4); // 뒷 4자리 번호
-    var pnum1 = phone_number.slice(0, -4); // 뒷 4자리 번호를 제외한 부분
-
-    for (var i = 0; i < pnum1.length; i++) {
-        answer += '*';
-    }
-
-    answer += pnum; // 가려지지 않은 뒷 4자리를 추가
     
-    return answer;
-}
+    for(let i = 0; i < phone_number.length - 4 ; i++) {
+    answer = answer + "*"
+    }
+    answer = answer + phone_number.slice(- 4);
+   
+    return answer; 
+    }
